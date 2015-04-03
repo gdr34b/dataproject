0398610a06ed5eb8a0460a1dc9d28dec9d18c99f
George Rush <gdr34b@mst.edu>
2015-04-03 12:46:38 -0500

    Save point.

--- orange_scripts/interpolation_test.py
+++ orange_scripts/interpolation_test.py
@@ -5,7 +5,30 @@ import Orange, sys, traceback
 ################################################################################
 # Function Definitions
 ################################################################################
-
+def getLeafNodes(node):
+    leafNodes = []
+    # null node
+    if not node:
+        pass
+    # internal node
+    elif node.branch_selector:
+        nodeDesc = node.branch_selector.class_var.name
+        for branch in node.branches:
+            leafNodes += getLeafNodes(branch)
+    # leaf node
+    else:
+        leafNodes.append(node)
+    return leafNodes
+    
+def getValueRanges(leaf):
+    valueRanges = []
+    
+    return valueRanges
+    
+def buildNewInstances(valueRanges):
+    newData = []
+    
+    return newData
 
 ################################################################################
 # Main Script
@@ -14,12 +37,13 @@ if __name__ == "__main__":
     try:
         irisData = Orange.data.Table("iris")
         treeClassifier = Orange.classification.tree.TreeLearner(irisData)
-        # TO DO:
-            # find leaf nodes and corresponding data instances
-            # collect value ranges (or all possible discrete values) for all instance attributes per leaf node
-            # build new instances with values in the available ranges (or discrete choices) for each leaf node
-            # build another data model including the new instances
-            # compare the new data model to the old one
+        leafNodes = getLeafNodes(treeClassifier.tree)
+        newData = []
+        for leaf in leafNodes:
+            valueRanges = getValueRanges(leaf)
+            newData += buildNewInstances(valueRanges)
+        # build another data model including the new instances
+        # compare the new data model to the old one
     except:
         traceback.print_exc(file=sys.stdout)
     
