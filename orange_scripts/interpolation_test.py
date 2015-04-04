9582044370bc46bbbd6e201ad69aca52a4c45a48
George Rush <gdr34b@mst.edu>
2015-04-04 10:50:29 -0500

    Finished interpolation test.

--- orange_scripts/interpolation_test.py
+++ orange_scripts/interpolation_test.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 
-import Orange, sys, traceback
+import math, Orange, random, sys, traceback
+
+################################################################################
+# Global Variables
+################################################################################
+NEW_DATA_PER_LEAF = 0.5
 
 ################################################################################
 # Function Definitions
@@ -21,29 +26,70 @@ def getLeafNodes(node):
     return leafNodes
     
 def getValueRanges(leaf):
-    valueRanges = []
+    if len(leaf.instances) > 0:
+        valueLists = []
+        for value in leaf.instances[0]:
+            valueLists.append([])
+        # get possible values
+        for item in leaf.instances:
+            for i in range(len(item)):
+                valueLists[i].append(item[i])
+        # build ranges from possible values
+        valueRanges = []
+        for eachList in valueLists:
+            # for continuous variables, store min and max data
+            if eachList[0].var_type == Orange.feature.Type.Continuous:
+                valueRanges.append({ \
+                    'type': Orange.feature.Type.Continuous, \
+                    'min': min(eachList), \
+                    'max': max(eachList)})
+            # else, store all possible values
+            else:
+                valueRanges.append({ \
+                    'type': eachList[0].var_type, \
+                    'values': set([str(x) for x in eachList])})
+        return valueRanges
     
-    return valueRanges
-    
-def buildNewInstances(valueRanges):
+def buildNewInstances(dataTable, leaf):
     newData = []
-    
+    domain = dataTable.domain
+    valueRanges = getValueRanges(leaf)
+    numOfNewInstances = int(math.floor(len(leaf.instances) * NEW_DATA_PER_LEAF))
+    for i in range(numOfNewInstances):
+        newInstance = []
+        for eachRange in valueRanges:
+            if eachRange['type'] == Orange.feature.Type.Continuous:
+                newInstance.append(random.uniform(eachRange['min'], eachRange['max']))
+            else:
+                newInstance.append(random.sample(eachRange['values'], 1)[0])
+        orangeInstance = Orange.data.Instance(domain, newInstance)
+        newData.append(orangeInstance)
     return newData
+    
+def addNewDataToTable(dataTable, newData):
+    for item in newData:
+        dataTable.append(item)
 
 ################################################################################
 # Main Script
 ################################################################################
 if __name__ == "__main__":
     try:
+        # load data into table
         irisData = Orange.data.Table("iris")
-        treeClassifier = Orange.classification.tree.TreeLearner(irisData)
+        # create decision tree
+        treeClassifier = Orange.classification.tree.TreeLearner(irisData, store_instances=True)
+        # make new data and add it to the current table
         leafNodes = getLeafNodes(treeClassifier.tree)
         newData = []
         for leaf in leafNodes:
-            valueRanges = getValueRanges(leaf)
-            newData += buildNewInstances(valueRanges)
-        # build another data model including the new instances
-        # compare the new data model to the old one
+            newData += buildNewInstances(irisData, leaf)
+        addNewDataToTable(irisData, newData)
+        # build another decision tree including the new instances
+        newTreeClassifier = Orange.classification.tree.TreeLearner(irisData, store_instances=True)
+        # somehow compare the new data model to the old one
+        print("*** Old Decision Tree ***\n" + str(treeClassifier) + \
+            "\n*** New Decision Tree ***\n" + str(newTreeClassifier))
     except:
         traceback.print_exc(file=sys.stdout)
     
